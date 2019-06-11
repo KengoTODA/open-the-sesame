564520700a74325d45116c813f1df1b8c9268aad
Kengo TODA <skypencil@gmail.com>
2019-06-11 16:16:14 +0800

    introduce sentry to the module on raspberry pi 3

--- pi/index.js
+++ pi/index.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 require("dotenv").config();
+const Sentry = require("@sentry/node");
+Sentry.init({ dsn: process.env.SENTRY_DSN });
+
 const signale = require("signale");
 signale.config({
   displayTimestamp: true,
